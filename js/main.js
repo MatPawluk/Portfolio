0276a4abf2c372a135f8a9a34ff4153e51c8aeff
Mateusz Pawluk <pawluk1988@icloud.com>
2024-08-09 11:51:01 +0200

    Background Hero text

---
+++ js/main.js
@@ -0,0 +1,61 @@
+var $tickerWrapper = $(".hero__name");
+var $list = $tickerWrapper.find("ul.list");
+var $clonedList = $list.clone();
+var listWidth = 10;
+
+$list.find("li").each(function (i) {
+  listWidth += $(this, i).outerWidth(true);
+});
+
+var endPos = $tickerWrapper.width() - listWidth;
+
+$list.add($clonedList).css({
+  width: listWidth + "px",
+});
+
+$clonedList.addClass("cloned").appendTo($tickerWrapper);
+
+//TimelineMax
+var infinite = new TimelineMax({ repeat: -1, paused: true });
+var time = 100;
+
+infinite
+  .fromTo(
+    $list,
+    time,
+    { rotation: 0.01, x: 0 },
+    { force3D: true, x: -listWidth, ease: Linear.easeNone },
+    0
+  )
+  .fromTo(
+    $clonedList,
+    time,
+    { rotation: 0.01, x: listWidth },
+    { force3D: true, x: 0, ease: Linear.easeNone },
+    0
+  )
+  .set($list, { force3D: true, rotation: 0.01, x: listWidth })
+  .to(
+    $clonedList,
+    time,
+    { force3D: true, rotation: 0.01, x: -listWidth, ease: Linear.easeNone },
+    time
+  )
+  .to(
+    $list,
+    time,
+    { force3D: true, rotation: 0.01, x: 0, ease: Linear.easeNone },
+    time
+  )
+  .progress(1)
+  .progress(0)
+  .play();
+
+//Pause/Play
+$tickerWrapper
+  .on("mouseenter", function () {
+    infinite.pause();
+  })
+  .on("mouseleave", function () {
+    infinite.play();
+  });
