015855edf8a37b1fb8ccb12f49da76306ba45c11
MatPawluk <pawluk1988@icloud.com>
2024-08-10 11:37:32 +0200

    logos

--- js/main.js
+++ js/main.js
@@ -98,3 +98,6 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 });
+
+// var copy = document.querySelector(".logos__slide").cloneNode(true);
+// document.querySelector(".logos").appendChild(copy);
